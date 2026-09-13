d1901c47bf513ec0272822722d999d3ed798224b
Praveena <hipraveenakiran@gmail.com>
2026-09-13 16:24:29 +0530

    fix LinkedIn link

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -10,8 +10,8 @@ const LINKS = [
   },
   {
     label: 'LinkedIn',
-    href: 'www.linkedin.com/in/praveena-kiran-a8791b2b9',
-    meta: 'www.linkedin.com/in/praveena-kiran-a8791b2b9',
+    href: 'https://www.linkedin.com/in/praveena-kiran-a8791b2b9',
+    meta: 'https://www.linkedin.com/in/praveena-kiran-a8791b2b9',
     external: true,
   },
   {
